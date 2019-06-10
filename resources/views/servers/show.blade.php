6576b87be39f3b2606ca79de9a6a85cb73c5e936
Shaun <shaunwah.zy@gmail.com>
2019-06-10 22:16:04 +0800

    Rework design

--- resources/views/servers/show.blade.php
+++ resources/views/servers/show.blade.php
@@ -6,10 +6,65 @@
 @endsection
 @section('title', $server->name)
 @section('content')
+<style>
+#hero-wrapper {
+    background: url('{{ $server->pings->last()->favicon }}') center center;
+}
+
+#hero {
+    background: rgba(52, 58, 64, 0.9);
+}
+</style>
+{{-- Title --}}
+<div id="hero-wrapper">
+    <div class="container-fluid mt-n4 mb-4" id="hero">
+        <div class="container py-5 text-white">
+            <div class="row align-items-center">
+                <div class="col-auto mr-auto">
+                    <h1 class="font-weight-bold">
+                        {{ $server->name }}
+                        <small class="d-block text-white-50">{{ $server->host . ($server->port != 25565 ? ':' . $server->port : '') }}</small>
+                    </h1>
+                    <ul class="list-inline">
+                        <li class="list-inline-item"><span class="font-weight-bold">{{ __('Rank') }}</span>&nbsp;
+                            {{ number_format($server->rank) }}
+                        </li>
+                        <li class="list-inline-item"><span class="font-weight-bold">{{ __('Score') }}</span>&nbsp;
+                            {{ number_format($server->score, 2) }}
+                        </li>
+                        @if ($server->pings->last()->status)
+                            <li class="list-inline-item">
+                                <span class="font-weight-bold">{{ __('Players') }}</span>&nbsp;
+                                {{ number_format($server->pings->last()->players_current) }}
+                                <span class="text-muted">/</span>
+                                {{ number_format($server->pings->last()->players_total) }}
+                            </li>
+                        @endif
+                        <li class="list-inline-item"><span class="font-weight-bold">{{ __('Votes') }}</span>&nbsp;
+                            {{ number_format($voteCountThisMonth) }}
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-auto">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+{{-- Information --}}
+@component('partials.alert')
+@endcomponent
 <div class="container">
-    <div class="row mb-3 align-items-center">
+    <div class="row alight-items-center">
         <div class="col-auto mr-auto">
-            <a class="text-decoration-none" href="{{ route('servers.index') }}"><i class="fal fa-chevron-left fa-fw"></i> Servers</a>
+            <h3>{{ __('Information') }}</h3>
+            <p>
+                @isset($server->description)
+                    {{ $server->description }}
+                @else
+                    <span class="text-muted">{{ __('No description set.') }}</span>
+                @endisset
+            </p>
         </div>
         <div class="col-auto">
             <a class="btn btn-primary btn-sm" href="{{ route('servers.votes.create', $server->id) }}" role="button"><i class="fal fa-check fa-fw"></i> Vote</a>
@@ -19,79 +74,8 @@
             @endcan
         </div>
     </div>
-    <div class="card mb-3" style="min-height:32rem;">
-        <div class="card-body">
-            <h1 class="card-title font-weight-bold">
-                {{ $server->name }}
-                <small class="text-muted d-block">
-                    {{ $server->host }}{{ $server->port != 25565 ? ':' . $server->port : '' }}
-                </small>
-            </h1>
-            <h2 class="d-none d-md-block display-3 text-muted" id="card-rank">
-                <sup><i class="fal fa-hashtag fa-xs fa-fw"></i></sup>{{ number_format($server->rank, 0, '.', ',') }}
-            </h2>
-            <ul class="list-inline mb-1">
-                @if ($server->pings->last()->status)
-                    <li class="list-inline-item">
-                        <span class="font-weight-bold">{{ __('Players') }}</span>&nbsp; {{ number_format($server->pings->last()->players_current, 0, '.', ',') }}
-                        <span class="text-muted">/</span>
-                        {{ number_format($server->pings->last()->players_total, 0, '.', ',') }}
-                    </li>
-                @endif
-                <li class="list-inline-item"><span class="font-weight-bold">
-                    {{ __('Score') }}</span>&nbsp; {{ number_format($server->score, 2, '.', ',') }}
-                </li>
-                <li class="list-inline-item"><span class="font-weight-bold">
-                    {{ __('Votes') }}</span>&nbsp; {{ number_format($voteCountThisMonth, 0, '.', ',') }}
-                </li>
-            </ul>
-            <hr>
-            <div class="row">
-                <div class="col-md-10 order-1 order-md-0">
-                    <div class="tab-content" id="tabContent">
-                        {{-- Description Tab --}}
-                        <div class="tab-pane show active" id="information" role="tabpanel" aria-labelledby="information-tab">
-                            <h5 class="card-subtitle mb-2 text-muted">{{ __('Description') }}</h5>
-                            <p class="card-text">
-                                @isset($server->description)
-                                    {{ $server->description }}
-                                @else
-                                    <span class="text-muted">{{ __('No description set.') }}</span>
-                                @endisset
-                            </p>
-                        </div>
-
-                        {{-- Statistics Tab --}}
-                        <div class="tab-pane" id="statistics" role="tabpanel" aria-labelledby="statistics-tab">
-                        <h5 class="card-subtitle mb-2 text-muted">{{ __('Player Count') }}</h5>
-                        <canvas id="canvas-player-history" height="128px"></canvas>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-2 order-0 order-md-1 mb-3 mb-md-0">
-                    <div class="nav flex-column nav-pills text-left text-md-right" id="tab" role="tablist" aria-orientation="vertical">
-                        <a class="nav-link active" id="information-tab" data-toggle="pill" href="#information" role="tab" aria-controls="information" aria-selected="true">{{ __('Information') }}
-                            <i class="fal fa-question-circle fa-fw"></i>
-                        </a>
-                        <a class="nav-link" id="statistics-tab" data-toggle="pill" href="#statistics" role="tab" aria-controls="statistics" aria-selected="false">{{ __('Statistics') }}
-                            <i class="fal fa-chart-line fa-fw"></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <img src="{{ $server->pings->last()->favicon }}" class="rounded" id="card-favicon" alt="{{ __('Favicon') }}">
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-auto mr-auto">
-            <small class="text-muted" data-toggle="tooltip" data-placement="top" title="{{ __('Server Status') }}"><i class="fas {{ $server->pings->last()->status == 1 ? 'fa-check-circle text-success' : 'fa-times-circle text-danger' }} fa-fw"></i> {{ Carbon\Carbon::parse($server->pings->last()->created_at)->diffForHumans() }}</small>
-        </div>
-        <div class="col-auto">
-            <a href="{{ url('/servers/versions/' . $server->version->slug) }}" class="badge badge-pill badge-danger">{{ $server->version->name }}</a>
-            <a href="{{ url('/servers/types/' . $server->type->slug) }}" class="badge badge-pill badge-success">{{ $server->type->name }}</a>
-            <a href="{{ url('/servers/countries/' . $server->country->slug) }}" class="badge badge-pill badge-primary">{{ $server->country->name }}</a>
-        </div>
-    </div>
+    <h3>{{ __('Player Count') }}</h3>
+    <canvas id="canvas-player-history"></canvas>
 </div>
 @endsection
 @section('scripts')
