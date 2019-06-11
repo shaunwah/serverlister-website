ac306fadf4ecf218e0fc6ad63a3ec5796c47cd0b
Shaun <shaunwah.zy@gmail.com>
2019-06-11 14:29:59 +0800

    Remove badges from server list

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -71,13 +71,8 @@
                                     <img src="{{ $server->pings->last()->favicon }}" class="rounded" alt="Favicon" height="48px" width="48px">
                                 </div>
                                 <div class="col-6 col-sm-7 col-md-8 text-left text-truncate">
-                                    <a class="font-weight-bold" href="{{ route('servers.show', $server->id) }}">{{ $server->name }}</a>
-                                    <span class="text-muted">{{ $server->host . ($server->port != 25565 ? ':' . $server->port : '') }}</span>
-                                    <span class="d-block">
-                                        <a href="{{ url('/servers/versions/' . $server->version->slug) }}" class="badge badge-pill badge-danger">{{ $server->version->name }}</a>
-                                        <a href="{{ url('/servers/types/' . $server->type->slug) }}" class="badge badge-pill badge-success">{{ $server->type->name }}</a>
-                                        <a href="{{ url('/servers/countries/' . $server->country->slug) }}" class="badge badge-pill badge-primary">{{ $server->country->name }}</a>
-                                    </span>
+                                    <a class="font-weight-bold" href="{{ route('servers.show', $server->id) }}">{{ $server->name }}</a> <span class="text-muted">{{ $server->version->name }}</span>
+                                    <span class="d-block">{{ $server->host . ($server->port != 25565 ? ':' . $server->port : '') }}</span>
                                 </div>
                                 <div class="col-3 col-sm-2 text-right">
                                     @if ($server->pings->last()->status == 1)
