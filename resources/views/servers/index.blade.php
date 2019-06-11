22e68c97b40de775674d59a5ebffd9b3e129213e
Shaun <shaunwah.zy@gmail.com>
2019-06-11 21:27:44 +0800

    Ignore errors for server favicon

--- resources/views/servers/index.blade.php
+++ resources/views/servers/index.blade.php
@@ -52,7 +52,7 @@
                                     <i class="fal fa-hashtag fa-fw"></i> {{ number_format($server->rank) }}
                                 </div>
                                 <div class="col-3 col-sm-2 col-md-1 text-left text-md-center">
-                                    <img src="{{ $server->pings->where('status', true)->last()->favicon }}" class="rounded" alt="Favicon" height="48px" width="48px">
+                                    <img src="{{ @$server->pings->where('status', true)->last()->favicon }}" class="rounded" alt="Favicon" height="48px" width="48px">
                                 </div>
                                 <div class="col-6 col-sm-7 col-md-8 text-left text-truncate">
                                     <a class="font-weight-bold" href="{{ route('servers.show', $server->id) }}">{{ $server->name }}</a> <span class="text-muted">{{ $server->version->name }}</span>
