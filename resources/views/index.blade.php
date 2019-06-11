54fe7c48e71e9a8deed6617c7a51085bf20afa0e
Shaun <shaunwah.zy@gmail.com>
2019-06-11 17:03:42 +0800

    Add player tracking data

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,56 +7,9 @@
 @section('content')
 <div class="container">
     <div class="jumbotron mb-3">
-        <h1 class="display-4">Hey there.</h1>
-        <p class="lead">ServerLister helps you to find the best Minecraft servers out there.</p>
-        <a class="btn btn-primary btn-lg" href="{{ route('servers.index') }}" role="button">Let's Go <i class="fal fa-arrow-right fa-fw"></i></a>
-    </div>
-    <div class="row mb-3">
-        <div class="col-md-4">
-            <div class="card mb-3 mb-md-0">
-                <div class="card-body">
-                    <div class="row align-items-center">
-                        <div class="col-auto mr-auto">
-                            <span class="text-muted">{{ __('Servers') }}</span>
-                            <h4 class="text-monospace">{{ number_format(App\Server::count()) }}</h4>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fal fa-server fa-fw fa-2x text-muted"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card mb-3 mb-md-0">
-                <div class="card-body">
-                    <div class="row align-items-center">
-                        <div class="col-auto mr-auto">
-                            <span class="text-muted">{{ __('Server Pings') }}</span>
-                            <h4 class="text-monospace">{{ number_format(App\ServerPing::count()) }}</h4>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fal fa-users fa-fw fa-2x text-muted"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card mb-3 mb-md-0">
-                <div class="card-body">
-                    <div class="row align-items-center">
-                        <div class="col-auto mr-auto">
-                            <span class="text-muted">{{ __('Votes') }}</span>
-                            <h4 class="text-monospace">{{ number_format(App\ServerVote::count()) }}</h4>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fal fa-check fa-fw fa-2x text-muted"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <h1 class="display-4">ServerLister</h1>
+        <p class="lead">Tracking {{ number_format(App\Server::count()) }} servers and {{ number_format(App\ServerPing::pluck('players_current', 'server_id')->sum()) }} players.</p>
+        <a class="btn btn-primary btn-lg" href="{{ route('servers.index') }}" role="button">Find Servers <i class="fal fa-arrow-right fa-fw"></i></a>
     </div>
     <h2 class="font-weight-bold">Popular Servers</h2>
             @if (App\Server::count() > 0)
