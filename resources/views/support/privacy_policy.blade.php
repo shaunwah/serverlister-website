28f2e9400f40c3bc5c773756b92e5f272e5d8f74
Shaun <shaunwah.zy@gmail.com>
2019-06-13 02:54:13 +0800

    Rewrite privacy policy

--- resources/views/support/privacy_policy.blade.php
+++ resources/views/support/privacy_policy.blade.php
@@ -3,38 +3,47 @@
 @section('content')
 <div class="container">
     <h1 class="font-weight-bold">Privacy Policy</h1>
-    <h3>Who we are.</h3>
+
     <p>
-        We are ServerLister, a Minecraft server list, a product of Zodurus Labs based in Singapore.
+        Your privacy is important to us.
+        It is Zodurus Labs' policy to respect your privacy regarding any information we may collect from you across <a href="{{ route('home') }}">our website</a> and other sites we own and operate.
     </p>
 
-    <h3>What personal data we collect.</h3>
-    <ul>
-        <li>
-            When you create an account, we may collect your email address, and IP address.
-        </li>
-        <li>
-            When you vote for a server, we may collect your IP address.
-        </li>
-    </ul>
+    <p>
+        We only ask for personal information when we truly need it to provide a service to you.
+        We collect it by fair and lawful means, with your knowledge and consent.
+        We also let you know why we’re collecting it and how it will be used.
+    </p>
 
-    <h3>How we use your personal data.</h3>
-    <ul>
-        <li>
-            Your personal data collected during account creation helps us to keep you updated through your email address on security updates and/or service notifications.
-            Your IP address allows us to determine your approximate location to help calibrate your search results within the website.
-        </li>
-        <li>
-            Your IP address collected during server voting helps us determine your location to determine a server's demographics.
-        </li>
-    </ul>
-    <h3>Who do we share your data with.</h3>
     <p>
-        We do not disclose your data to any third-parties.
+        We only retain collected information for as long as necessary to provide you with your requested service.
+        What data we store, we’ll protect within commercially acceptable means to prevent loss and theft,
+        as well as unauthorised access, disclosure, copying, use or modification.
     </p>
-    <h3>Where can I provide feedback?</h3>
+
     <p>
-        You may provide your valuable feedback through our <a href="{{ url('//discordapp.com/invite/nzqRgUw') }}" target="_blank">Discord server</a>.
+        We don’t share any personally identifying information publicly or with third-parties,
+        except when required to by law.
     </p>
+
+    <p>
+        Our website may link to external sites that are not operated by us.
+        Please be aware that we have no control over the content and practices of these sites,
+        and cannot accept responsibility or liability for their respective privacy policies.
+    </p>
+
+    <p>
+        You are free to refuse our request for your personal information,
+        with the understanding that we may be unable to provide you with some of your desired services.
+    </p>
+
+    <p>
+        Your continued use of our website will be regarded as acceptance of our practices around privacy and personal information.
+        If you have any questions about how we handle user data and personal information, feel free to contact us through our <a href="{{ url('//discordapp.com/invite/nzqRgUw') }}" target="_blank">Discord server</a>.
+    </p>
+
+    <span class="text-muted">
+        Effective as at 1 June 2019
+    </span>
 </div>
 @endsection
