611176122789c23b7da2d6c35c541b0abc54df17
Shaun <shaunwah.zy@gmail.com>
2019-06-13 02:02:27 +0800

    Remove unused classes

--- app/Http/Requests/UpdateServer.php
+++ app/Http/Requests/UpdateServer.php
@@ -3,7 +3,6 @@
 namespace App\Http\Requests;
 
 use Illuminate\Validation\Rule;
-use Illuminate\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateServer extends FormRequest
