a5d5f6192b4a550c0f9fbe038a2b238a18069863
Shaun <shaunwah.zy@gmail.com>
2019-06-13 00:16:42 +0800

    Create ReportController.php

---
+++ app/Http/Controllers/ReportController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Server;
+use App\Report;
+use App\Http\Requests\StoreReport;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+
+class ReportController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('reports.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreReport $request, Report $report)
+    {
+        $validated = $request->validated();
+
+        $validated['user_id'] = auth()->id();
+
+        $result = $report->create($validated);
+        $report = Report::find($result->id);
+        $report->{Str::plural(request()->entity)}()->attach(request()->entity_id);
+
+        session()->flash('alert_colour', 'success');
+        session()->flash('alert', 'You have successfully submitted a report.');
+
+        return redirect('/dashboard');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Report  $report
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Report $report)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Report  $report
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Report $report)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Report  $report
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Report $report)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Report  $report
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Report $report)
+    {
+        //
+    }
+}
