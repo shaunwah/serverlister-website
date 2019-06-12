c62fbad4d73661e3060f1bb509b367804150a466
Shaun <shaunwah.zy@gmail.com>
2019-06-12 13:32:39 +0800

    Remove AMP support

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,33 +19,11 @@
 
     <title>@yield('title', 'Page') - ServerLister</title>
 
-    <script async src="https://cdn.ampproject.org/v0.js"></script>
-    <script src="https://kit.fontawesome.com/ef9f9fad9d.js"></script>
-
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/flag-icon.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 
-    <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
-
-    <script type="application/ld+json">
-    {
-        "@context": "http://schema.org",
-        "@type": "Webpage",
-        "url": "{{ url()->current() }}",
-        "name": "ServerLister",
-        "headline": "@yield('title', 'Page') - ServerLister",
-        "description": "ServerLister helps you find the best Minecraft servers in the wild.",
-        "mainEntityOfPage": {
-            "@type": "Webpage",
-            "@id": "{{ url()->current() }}"
-        },
-        "publisher": {
-            "@type": "Organization",
-            "name": "Zodurus Labs",
-        },
-    }
-    </script>
+    <script src="https://kit.fontawesome.com/ef9f9fad9d.js"></script>
 </head>
 <body>
     <div id="app">
