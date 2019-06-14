82f132cd7aa1a714b1b607c65c471fb240a11583
Shaun <shaunwah.zy@gmail.com>
2019-06-15 01:20:14 +0800

    Create GoogleReCaptcha.php

---
+++ app/Utilities/GoogleReCaptcha.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Utilities;
+
+class GoogleReCaptcha
+{
+    public static function validateResponse()
+    {
+        $url = "https://www.recaptcha.net/recaptcha/api/siteverify";
+
+        $data = [
+            'secret' => env('GOOGLE_RECAPTCHA_SECRET'),
+            'response' => request('g-recaptcha-response'),
+            'remoteip' => request()->ip(),
+        ];
+
+        $options = [
+            'http' => [
+                'header' => ('Content-type: application/x-www-form-urlencoded' . "\r\n"),
+                'method' => 'POST',
+                'content' => http_build_query($data),
+            ],
+        ];
+
+        $context = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        $resultJson = json_decode($result);
+
+        return $resultJson->success;
+    }
+}
