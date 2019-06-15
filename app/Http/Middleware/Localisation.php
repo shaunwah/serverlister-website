5092707b4d1d63d7f1f9ae2bd4e6028cb93b5a6f
Shaun <shaunwah.zy@gmail.com>
2019-06-15 19:01:55 +0800

    Create Localisation.php

---
+++ app/Http/Middleware/Localisation.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Localisation
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (session()->has('locale'))
+        {
+            app()->setLocale(session()->get('locale'));
+        }
+
+        return $next($request);
+    }
+}
