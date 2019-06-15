b44fb3828070a74dd93f67c34c9338facee83956
Shaun <shaunwah.zy@gmail.com>
2019-06-15 10:49:56 +0800

    Remove meta robots

--- resources/views/servers/votes/create.blade.php
+++ resources/views/servers/votes/create.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.app')
 @section('meta_description', __('Vote for :server_name, a :server_version :server_type-based Minecraft server (:server_ip_address) located in :server_country.', ['server_name' => $server->name, 'server_version' => $server->version->name, 'server_type' => $server->type->name, 'server_country' => $server->country->name, 'server_ip_address' => $server->host . ($server->port != 25565 ? ':' . $server->port : '')]))
-@section('meta_robots', 'noindex')
 @section('head')
 {{-- ReCaptcha --}}
 <script src="https://www.google.com/recaptcha/api.js?render={{ env('GOOGLE_RECAPTCHA_SITE') }}"></script>
