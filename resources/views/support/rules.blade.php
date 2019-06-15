bc6c82ceea856188ea3c1d68098797ec426c19ca
Shaun <shaunwah.zy@gmail.com>
2019-06-15 16:58:31 +0800

    Fix typo

--- resources/views/support/rules.blade.php
+++ resources/views/support/rules.blade.php
@@ -2,7 +2,7 @@
 @section('title', __('support.text.headers.rules'))
 @section('content')
 <div class="container">
-    <h1 class="font-weight-bold">__('support.text.headers.rules')</h1>
+    <h1 class="font-weight-bold">{{ __('support.text.headers.rules') }}</h1>
 
     <p>
         In addition to the <a href="{{ url('/support/terms-of-service') }}">Terms of Service</a> laid down by <a href="{{ route('home') }}">ServerLister</a>, your access to this website is governed by the rules as stated. Non-compliance may restrict your access to this website and its services.
