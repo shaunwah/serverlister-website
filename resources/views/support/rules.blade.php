bf7f6a1941a61d6c1cf5e9c15fe155cabf4f16fd
Shaun <shaunwah.zy@gmail.com>
2019-06-28 18:54:01 +0800

    Update rules.blade.php

--- resources/views/support/rules.blade.php
+++ resources/views/support/rules.blade.php
@@ -15,13 +15,12 @@
     <p>
         Any user may list their server on our website, provided that it adheres to the following regulations:
         <ol type="a">
-            <li>The server may not be running offline or requires a VPN to connect.</li>
+            <li>The server may not be require a VPN to connect.</li>
             <li>The server may not be promoting illegal content, including drug abuse and pornography.</li>
             <li>The server may not be listed more than once.</li>
             <li>The server may not be part of a hub. Only the main node of a hub may be listed.</li>
             <li>The server may infringe the Minecraft End User Licensing Agreement (EULA).</li>
             <li>The server may infringe copyrights held by Microsoft Corporation and of its affiliates.</li>
-            <li>The server may infringe laws in the Republic of Singapore.</li>
         </ol>
     </p>
 
