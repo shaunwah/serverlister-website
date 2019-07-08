6291d296813ec4d607ddaaeea98304d38d5452b0
Shaun <shaunwah.zy@gmail.com>
2019-07-09 02:44:06 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,13 +30,13 @@
     </script>
 
     {{-- Google AdSense --}}
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+{{--     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <script>
         (adsbygoogle = window.adsbygoogle || []).push({
         google_ad_client: "ca-pub-8259485254455968",
         enable_page_level_ads: true
         });
-    </script>
+    </script> --}}
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/flag-icon.min.css') }}">
